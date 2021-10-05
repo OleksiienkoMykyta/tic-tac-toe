2aa264bd02b785d035a385cea61d6e8ef9446487
OleksiienkoMykyta <mykyta.oleksiienko@gmail.com>
2021-10-05 14:17:02 +0300

    recurs bug fixed

--- src/main.js
+++ src/main.js
@@ -120,6 +120,7 @@ function app() {
     win();
 
     function setStorage() {
+        localStorage.clear();
         const str = JSON.stringify(cells);
         localStorage.setItem('cells', str);
     }
@@ -128,7 +129,9 @@ function app() {
         const getStr = localStorage.getItem('cells');
         cells = JSON.parse(getStr);
         builder(cells);
-        undoSwitcher(cells.length);
+        if (cells !== null) {
+            undoSwitcher(cells.length);
+        }
     }
 
     function cellsAdder(eventTarg, cellVal) {
@@ -161,7 +164,9 @@ function app() {
     function builder() {
         dump();
 
-        if (cells.length !== null) {
+        if (cells !== null) {
+            console.log('проверка пройдена');
+            console.log(cells);
             for (let i = 0; i < cells.length; i += 1) {
                 if (cells[i].idVal === '') {
                     cells.pop();
@@ -170,8 +175,6 @@ function app() {
             for (let i = 0; i < cells.length; i += 1) {
                 document.querySelector(`#${cells[i].idVal}`).classList.add(`${cells[i].fieldVal}`);
             }
-            localStorage.clear();
-            setStorage();
         }
     }
 
@@ -225,6 +228,7 @@ function app() {
             }
         }
         builder(cells, e.target.id);
+        setStorage();
         undoSwitcher(cells.length);
         win();
     });
@@ -233,12 +237,14 @@ function app() {
         undoSwitcher(cells.length);
         undo(cells);
         builder(cells);
+        setStorage();
     })
 
     document.querySelector('.redo-btn').addEventListener('click', () => {
         redoSwitcher(undoStorage.length);
         redo(undoStorage);
         builder(cells);
+        setStorage();
         win();
     })
 
@@ -248,227 +254,10 @@ function app() {
 
     window.addEventListener('storage', () => {
         getStorage();
-        builder(cells);
         win();
     })
-
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     app();
 });
-
-//function app() {
-//     let cells = [];
-//     let undoStorage = [];
-//     if (localStorage.getItem('cells') !== null) {
-//         getStorage();
-//     }
-//
-//     function cellsDisable() {
-//         field.style.pointerEvents = 'none';
-//
-//     }
-//
-//     function getWinStatus(num1, num2, num3, winClass, textNode) {
-//         let inputNode = document.createTextNode(textNode);
-//         document.querySelector('.won-title').classList = 'won-title';
-//         document.querySelector('.won-message').appendChild(inputNode);
-//         document.querySelector(`#c-${num1}`).classList.add('win', `${winClass}`);
-//         document.querySelector(`#c-${num2}`).classList.add('win', `${winClass}`);
-//         document.querySelector(`#c-${num3}`).classList.add('win', `${winClass}`);
-//         document.createTextNode(textNode);
-//         cellsDisable();
-//     }
-//
-// // ========================================================
-//     function win() {
-//         let cellsArr = document.querySelectorAll('.cell');
-//         cellsArr = Array.from(cellsArr);
-//         cellsArr.sort((a, b) => {
-//             return cellsId(a) - cellsId(b);
-//         });
-//
-//         function horizontalWin(cellsArr) {
-//             let rowLength = Math.sqrt(cellsArr.length);
-//             let horizontalWinner = true;
-//             for (let i = 0; i < cellsArr.length; i += rowLength) {
-//                 let firstCellType = cellsType(cellsArr[i]);
-//                 if (!firstCellType) {
-//                     continue;
-//                 }
-//                 for (let j = i; j < i + rowLength; j += 1) {
-//                     if (firstCellType !== cellsArr[i]) {
-//                         horizontalWinner = false;
-//                         break;
-//                     }
-//                 }
-//                 if (horizontalWinner) {
-//                     return true;
-//                 }
-//             }
-//         }
-//
-//         horizontalWin(cellsArr);
-//
-//         function cellsId(node) {
-//             let id = node.id.split('-')[1];
-//             return Number.parseInt(id);
-//         }
-//
-//         function cellsType(cellsArr) {
-//             if (cellsArr.classList.contains('ch')) {
-//                 return 'ch';
-//             }
-//             if (cellsArr.classList.contains('r')) {
-//                 return 'r';
-//             }
-//             return  null;
-//         }
-//
-//
-//     }
-//
-// // ========================================================
-//     win();
-//
-//     function setStorage() {
-//         const str = JSON.stringify(cells);
-//         localStorage.setItem('cells', str);
-//     }
-//
-//     function getStorage() {
-//         const getStr = localStorage.getItem('cells');
-//         cells = JSON.parse(getStr);
-//         builder(cells);
-//         undoSwitcher(cells.length);
-//     }
-//
-//     function cellsAdder(eventTarg, cellVal) {
-//         cells.push({
-//             idVal: eventTarg.id,
-//             fieldVal: cellVal,
-//         });
-//     }
-//
-//     function restart() {
-//         localStorage.clear();
-//         dump();
-//         cells = [];
-//         undoStorage = [];
-//         if (document.querySelector('.won-message').childNodes[0] instanceof Text) {
-//             document.querySelector('.won-message').childNodes[0].remove();
-//             document.querySelector('.won-title').classList.add('hidden');
-//         }
-//         document.querySelector('.undo-btn').disabled = true;
-//         document.querySelector('.redo-btn').disabled = true;
-//         field.style.pointerEvents = 'auto';
-//     }
-//
-//     function dump() {
-//         for (let i = 0; i < 9; i += 1) {
-//             document.querySelector(`#c-${[i]}`).className = 'cell';
-//         }
-//     }
-//
-//     function builder() {
-//         dump();
-//
-//         if (cells.length !== null) {
-//             for (let i = 0; i < cells.length; i += 1) {
-//                 if (cells[i].idVal === '') {
-//                     cells.pop();
-//                 }
-//             }
-//             for (let i = 0; i < cells.length; i += 1) {
-//                 document.querySelector(`#${cells[i].idVal}`).classList.add(`${cells[i].fieldVal}`);
-//             }
-//             localStorage.clear();
-//             setStorage();
-//         }
-//     }
-//
-//     function undo(cells) {
-//         if (cells.length > 0) {
-//             undoStorage.push(cells.pop());
-//         }
-//         if (document.querySelector('.won-message').childNodes[0] instanceof Text) {
-//             document.querySelector('.won-message').childNodes[0].remove();
-//             document.querySelector('.won-title').classList.add('hidden');
-//         }
-//         if (cells.length === 0) {
-//             document.querySelector('.undo-btn').disabled = true;
-//         }
-//         document.querySelector('.redo-btn').disabled = false;
-//         field.style.pointerEvents = 'auto';
-//     }
-//
-//     function redo(undoStorage) {
-//         if (undoStorage.length > 0) {
-//             cells.push(undoStorage.pop());
-//         }
-//         if (undoStorage.length === 0) {
-//             document.querySelector('.redo-btn').disabled = true;
-//         }
-//         document.querySelector('.undo-btn').disabled = false;
-//     }
-//
-//     function undoSwitcher(counter) {
-//         if (counter > 0) {
-//             document.querySelector('.undo-btn').disabled = false;
-//         } else {
-//             document.querySelector('.undo-btn').disabled = true;
-//         }
-//     }
-//
-//     function redoSwitcher(counter) {
-//         if (counter > 0) {
-//             document.querySelector('.redo-btn').disabled = false;
-//         } else {
-//             document.querySelector('.redo-btn').disabled = true;
-//         }
-//     }
-//
-//     field.addEventListener('click', (e) => {
-//         if (e.target.classList.contains('r') === false && e.target.classList.contains('ch') === false) {
-//             if (cells.length % 2 === 0) {
-//                 cellsAdder(e.target, 'ch')
-//             } else {
-//                 cellsAdder(e.target, 'r')
-//             }
-//         }
-//         builder(cells, e.target.id);
-//         undoSwitcher(cells.length);
-//         win();
-//     });
-//
-//     document.querySelector('.undo-btn').addEventListener('click', () => {
-//         undoSwitcher(cells.length);
-//         undo(cells);
-//         builder(cells);
-//     })
-//
-//     document.querySelector('.redo-btn').addEventListener('click', () => {
-//         redoSwitcher(undoStorage.length);
-//         redo(undoStorage);
-//         builder(cells);
-//         win();
-//     })
-//
-//     document.querySelector('.restart-btn').addEventListener('click', () => {
-//         restart();
-//     })
-//
-//     window.addEventListener('storage', () => {
-//         getStorage();
-//         builder(cells);
-//         win();
-//     })
-//
-//
-// }
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//     app();
-// });
